8b05798d551b2604b79f8c725b31da56d7b5e153
Felipe Soares <140653664+FelipeSoares-code@users.noreply.github.com>
2025-10-14 00:33:15 +0000

    iniciei a modelagem da tela de cadastro

--- src/telas/Cadastro.jsx
+++ src/telas/Cadastro.jsx
@@ -3,8 +3,20 @@
 export default function Cadastro(){
 
     return (
-        <section id="cadastro" className="">
-            <p>Cadastro</p>
-        </section>
+        <form id="cadastro" className="m-auto mt-15 w-3/4 flex flex-col gap-8 border-solid-var(--color-azul-padrao)">
+            <h1>Cadastro</h1>
+
+            <input id="nome" placeholder="Nome e Sobrenome:" type="text" />
+
+            <input id="email" placeholder="E-mail:" type="text" />
+
+            <input id="phone" placeholder="Telefone:" type="text" />
+
+            <input id="senha" placeholder="Senha:" type="text" />
+
+            <input id="repSenha" placeholder="Repetir Senha:" type="text" />
+
+            <button>Cadastrar</button>
+        </form>
     )
 }
