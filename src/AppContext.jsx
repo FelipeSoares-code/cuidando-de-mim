def8cda45fef25b8c222aea407fae91b1993a054
Felipe Soares <felipesilvasoares003@gmail.com>
2025-11-07 09:54:05 -0300

    mudando o vite.config para usar url customizada

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -3,20 +3,20 @@ import { createContext, useContext, useState } from "react";
 const AppContext = createContext();
 
 export function AppProvider({ children }) {
-    const [linkDoctoralia, setLinkDoctoralia] = useState("https://www.doctoralia.com.br/rita-soares/psicologo/sao-bernardo-do-campo")
+  const [linkDoctoralia, setLinkDoctoralia] = useState("https://www.doctoralia.com.br/rita-soares/psicologo/sao-bernardo-do-campo")
 
-    const [pathPadrao, setPathPadrao] = useState("/cuidando-de-mim/")
+  const [pathPadrao, setPathPadrao] = useState("/cuidando-de-mim/")
 
-    return (
-        <AppContext.Provider 
-          value={{
-            linkDoctoralia, setLinkDoctoralia, 
-            pathPadrao, setPathPadrao
-          }}
-        >
-          {children}
-        </AppContext.Provider>
-    );
+  return (
+    <AppContext.Provider 
+      value={{
+        linkDoctoralia, setLinkDoctoralia, 
+        pathPadrao, setPathPadrao
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 export function useApp() {
